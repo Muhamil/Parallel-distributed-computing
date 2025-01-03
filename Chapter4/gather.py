5e28ca6a6d398ed7a9d31dc55d1a865d633c24ba
Muhamil <muhamil.hussain@gmail.com>
2025-01-04 00:48:32 +0500

    Gather

---
+++ Chapter4/gather.py
@@ -0,0 +1,13 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+data = (rank + 1) ** 2
+
+gathered_data = comm.gather(data, root=0)
+if rank == 0:
+    print(f"Rank {rank} receiving data from other processes:")
+    for i in range(1, size):
+        value = gathered_data[i]
+        print(f"Process {rank} received {value} from process {i}")
